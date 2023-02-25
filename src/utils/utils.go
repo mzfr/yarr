908171bacc6b86d159aa1a362c3fae57a46d0ce6
mzfr <hello@mzfr.me>
2023-02-25 17:08:57 +0530

    Create a utils package

---
+++ src/utils/utils.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func parseAuthfile(authfile io.Reader) (username, password string, err error) {
+	scanner := bufio.NewScanner(authfile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			return "", "", fmt.Errorf("wrong syntax (expected `username:password`)")
+		}
+		username = parts[0]
+		password = parts[1]
+		break
+	}
+	return username, password, nil
+}
+
+func getPocketAuth() (key, token string, err error) {
+	var pocketfile string
+	configPath, err := os.UserConfigDir()
+
+	storagePath := filepath.Join(configPath, "yarr")
+	pocketfile = filepath.Join(storagePath, "pocket.txt")
+	f, err := os.Open(pocketfile)
+	if err != nil {
+		log.Fatal("Failed to open auth file: ", err)
+	}
+	defer f.Close()
+
+	key, token, err = parseAuthfile(f)
+
+	if err != nil {
+		log.Fatal("Failed to parse auth file: ", err)
+	}
+
+	return key, token, err
+}
